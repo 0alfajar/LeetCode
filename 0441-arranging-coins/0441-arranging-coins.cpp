d14cc055ad55a30e557a54bd9d1ba20bf66db5fe
Alfajar <115224102+xa-4104@users.noreply.github.com>
2023-10-07 23:52:18 +0700

    Time: 2 ms (71.44%), Space: 6.2 MB (23.61%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -1,13 +1,22 @@
+//class Solution {
+//public:
+//    int arrangeCoins(int n) {
+//        int k = 0;
+//        while(n > 0){
+//            k++;
+//            n -= k;
+//        }
+//        if(n == 0) return k;
+//        else return k -1;
+//    }
+//};
+
+//Alternatif Solution
+
 class Solution {
 public:
     int arrangeCoins(int n) {
-        int k = 0;
-        while(n > 0){
-            k++;
-            n -= k;
-        }
-        if(n == 0) return k;
-        else return k -1;
+        return (sqrt(8L * n + 1) - 1) / 2;
     }
 };
 
