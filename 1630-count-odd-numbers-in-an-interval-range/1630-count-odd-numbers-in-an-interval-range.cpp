3415e1995b7dd7cade8ee747f9c6e99525d46be9
Alfajar <115224102+xa-4104@users.noreply.github.com>
2023-10-10 19:42:10 +0700

    Time: 2000 ms (25.51%), Space: 6.3 MB (15.61%) - LeetHub

--- 1630-count-odd-numbers-in-an-interval-range/1630-count-odd-numbers-in-an-interval-range.cpp
+++ 1630-count-odd-numbers-in-an-interval-range/1630-count-odd-numbers-in-an-interval-range.cpp
@@ -3,9 +3,7 @@ public:
     int countOdds(int low, int high) {
         int count = 0;
         for(int i = low; i <= high; i++){
-            if(i % 2 == 1){
-                count++;
-            }
+            if(i % 2 == 1) count++;
         }
         return count;
     }
