948c7012ac0a5e2f632b334942cea0364ce50e8a
Alfajar <115224102+xa-4104@users.noreply.github.com>
2023-11-04 15:40:33 +0700

    Time: 7 ms (57.36%), Space: 8.5 MB (17.51%) - LeetHub

---
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string reverseVowels(string s) {
+       stack<char>st;
+       for(int i = 0; i < s.length(); i++){
+           if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U'){
+               st.push(s[i]);
+           }
+       }
+       for(int i = 0; i < s.length(); i++){
+           if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U'){
+               char c = st.top();
+               st.pop();
+               s[i] = c;
+           }
+       }
+       return s; 
+    }
+};
