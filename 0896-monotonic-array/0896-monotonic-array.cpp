77727fb2fbd5697db68549fcfe5bb801bcf69e43
Alfajar <115224102+xa-4104@users.noreply.github.com>
2024-01-02 13:40:57 +0700

    Time: 301 ms (5.15%), Space: 96.7 MB (95.71%) - LeetHub

---
+++ 0896-monotonic-array/0896-monotonic-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        bool increasing = true;
+        bool decreasing = true;
+
+        for(int i = 0; i < nums.size() - 1; i++){
+            if(nums[i] < nums[i+1]) decreasing = false;
+            else if(nums[i] > nums[i+1]) increasing = false;
+        }
+        return increasing || decreasing;
+    }
+};
