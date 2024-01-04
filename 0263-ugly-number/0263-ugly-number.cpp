f0ea60abe44a678e3e31438a319cab45504b4b60
Alfajar <115224102+xa-4104@users.noreply.github.com>
2024-01-04 13:15:17 +0700

    Time: 4 ms (24.86%), Space: 6.2 MB (75.63%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.cpp
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     bool isUgly(int n) {
-        if(n == 1) return true;
-        else if (n % 2 == 0 && n % 3 == 0 && n % 5 == 0) return true;
-        else if (n % 2 == 0 && n % 3 == 0) return true;
-        else if (n % 2 == 0 && n % 5 == 0) return true;
-        else if (n % 3 == 0 && n % 5 == 0) return true;
-        else return false;
+        if(n <= 0) return false;
+        while(n > 1){
+            if (n % 2 == 0) n /= 2;
+            else if (n % 3 == 0) n /= 3;
+            else if (n % 5 == 0) n /= 5;
+            else break;
+        }
+        return (n == 1);
     }
 };
