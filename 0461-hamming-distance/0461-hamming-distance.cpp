933aaf0e1ec819ff1e1e0110a752949a601b159a
Alfajar <115224102+xa-4104@users.noreply.github.com>
2024-01-05 13:58:05 +0700

    Time: 0 ms (100%), Space: 6.4 MB (17.25%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.cpp
+++ 0461-hamming-distance/0461-hamming-distance.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     int hammingDistance(int x, int y) {
-        return bitset<32>(x^y).count();
+        //return bitset<32>(x^y).count();
+        int result = x ^ y;
+        int count = 0;
+        while (result > 0){
+            count += result & 1;
+            result >>= 1;
+        }
+        return count;
     }
 };
