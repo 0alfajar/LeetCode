e1f43a3fad6b5adc2fd7254656b307c4da06bdd9
Alfajar <115224102+xa-4104@users.noreply.github.com>
2024-01-07 14:23:11 +0700

    Time: 4 ms (20.7%), Space: 7.6 MB (28.97%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -3,7 +3,7 @@ public:
     int numIdenticalPairs(vector<int>& nums) {
         int count = 0;
         for(int i = 0; i < nums.size(); i++){
-            for(int j = 0; j < i;j++){
+            for(int j = 0; j < i; j++){
                 if(nums[i] == nums[j]) count++;
             }
         }
