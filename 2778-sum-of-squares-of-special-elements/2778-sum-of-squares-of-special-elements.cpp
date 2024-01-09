cf1ceff1b6ebf830a55a098b6ccceb739d64ac4f
Alfajar <115224102+xa-4104@users.noreply.github.com>
2024-01-09 13:35:55 +0700

    Time: 16 ms (23.76%), Space: 35.9 MB (16.22%) - LeetHub

---
+++ 2778-sum-of-squares-of-special-elements/2778-sum-of-squares-of-special-elements.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int sumOfSquares(vector<int>& nums) {
+        int sum = 0;
+        for(int i = 1; i <= nums.size(); i++){
+            if(nums.size() % i == 0){
+                sum += nums[i - 1] * nums[i - 1];
+            }
+        }
+        return sum;
+    }
+};
