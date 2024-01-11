2bd424db3353cf3e9fb85101e5df34c0473f0e7c
Alfajar <115224102+xa-4104@users.noreply.github.com>
2024-01-11 14:15:20 +0700

    Time: 410 ms (13.04%), Space: 10.5 MB (96.91%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> sum;
         for(int i = 0; i < nums.size() - 1; i++){
             for(int j = i + 1; j < nums.size(); j++){
                 if(nums[i] + nums[j] == target){
-                    return {i,j};
+                    sum.push_back(i);
+                    sum.push_back(j);
                 }
             }
         }
-        return {};
+        return sum;
     }
 };
