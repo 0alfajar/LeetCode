388b0d203514eb6c1ad579eea25fd85851650670
Alfajar <115224102+xa-4104@users.noreply.github.com>
2024-01-12 14:48:11 +0700

    Time: 5 ms (31.78%), Space: 9.2 MB (63.63%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int maxCandies = *max_element(candies.begin(), candies.end());
+        vector<bool> res;
+        for(int i = 0; i < candies.size(); i++){
+            res.push_back(candies[i] + extraCandies >= maxCandies);
+        }
+        return res;
+    }
+};
