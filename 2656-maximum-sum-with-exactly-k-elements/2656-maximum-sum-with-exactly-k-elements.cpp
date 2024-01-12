efa8e15a1c6863226acccbc72df6cac897172c46
Alfajar <115224102+xa-4104@users.noreply.github.com>
2024-01-12 16:03:07 +0700

    Time: 36 ms (35.55%), Space: 71 MB (84.93%) - LeetHub

--- 2656-maximum-sum-with-exactly-k-elements/2656-maximum-sum-with-exactly-k-elements.cpp
+++ 2656-maximum-sum-with-exactly-k-elements/2656-maximum-sum-with-exactly-k-elements.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int maximizeSum(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
-        int curr = nums[nums.size() - 1];
+        int curr = *max_element(nums.begin(), nums.end());
         int sum = 0;
         for(int i = 1; i <= k; i++){
             sum += curr;
