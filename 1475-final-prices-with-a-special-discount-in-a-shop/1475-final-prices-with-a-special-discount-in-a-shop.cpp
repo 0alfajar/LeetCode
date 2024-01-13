61a7c164229a3e791f5b926194a1328ef547b8b7
Alfajar <115224102+xa-4104@users.noreply.github.com>
2024-01-13 15:00:26 +0700

    Time: 7 ms (29.63%), Space: 10.2 MB (63.95%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -5,7 +5,7 @@ public:
         for(int i = 0; i < prices.size(); i++){
             int disc = 0;
             for(int j = i + 1; j < prices.size(); j++){
-                if(prices[i] >= prices[j]){
+                if(prices[j] <= prices[i]){
                     disc = prices[j];
                     break;
                 }
