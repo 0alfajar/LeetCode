cb96892122f2aebddb13bc797e9c2b40821443a6
Alfajar <115224102+xa-4104@users.noreply.github.com>
2024-01-14 13:34:02 +0700

    Time: 7 ms (34.49%), Space: 10.1 MB (35.72%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        size_t middle = nums.size() / 2;
-        vector<int> firstHalf(nums.begin(), nums.begin() + middle);
-        vector<int> secondHalf(nums.begin() + middle, nums.end());
-        vector<int> shuffle;
-        for(int i = 0; i < middle; i++){
-            shuffle.push_back(firstHalf[i]);
-            shuffle.push_back(secondHalf[i]);
+        vector<int> ans;
+        for(int i = 0; i < nums.size() / 2; i++){
+            ans.push_back(nums[i]);
+            ans.push_back(nums[i + n]);
         }
-        return shuffle;
+        return ans;
     }
 };
