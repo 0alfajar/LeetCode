6884af10eb6cdcc19e5f8bd9b26f2e26600d3f62
Alfajar <115224102+0alfajar@users.noreply.github.com>
2026-09-24 15:35:06 +0700

    Time: 1 ms (72.27%), Space: 19.4 MB (5.72%) - LeetHub

--- 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.py
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.py
@@ -1,12 +1,14 @@
 class Solution:
     def smallestIndex(self, nums: List[int]) -> int:
-        sum_of_digits = [sum(map(int, str(num))) for num in nums]
-        idx = []
-        for i in range(len(nums)):
-            if i == sum_of_digits[i]:
-                idx.append(i)
-        if len(idx) > 0:
-            return min(idx)
-        else:
-            return -1
-
+        sum_nums = []
+        for num in nums:
+            num = str(num)
+            sum_digit = 0
+            for digit in num:
+                sum_digit += int(digit)
+            sum_nums.append(sum_digit)
+            
+        for i in range(len(sum_nums)):
+            if i == sum_nums[i]:
+                return i    
+        return -1
